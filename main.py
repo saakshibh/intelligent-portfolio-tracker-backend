0354d2b2fc883f00618da13ee1a81dac51d1dbb8
sakshi b h <saakshiibh@gmail.com>
2026-05-19 23:39:02 +0530

    fix: allow global cors origins for production deployment

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Enable CORS for Next.js
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
